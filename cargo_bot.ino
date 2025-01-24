340f9e30092e2c4a82581da655a4427bcf86b74c
Abbas Ali <abbasalinaqvi888@gmail.com>
2025-01-24 19:07:35 +0530

    New update on cargo_bot.ino

--- cargo_bot.ino
+++ cargo_bot.ino
@@ -1,4 +1,4 @@
-#include <Servo.h>
+#include <Servo.h> 
 
 #define ENA 9
 #define IN1 2
@@ -15,6 +15,9 @@
 
 #define SERVO_PIN 13
 
+#define LED_LEFT 6     // Left LED pin
+#define LED_RIGHT 14   // Right LED pin
+
 Servo myServo;
 
 void setup() {
@@ -24,24 +27,30 @@ void setup() {
   pinMode(ENB, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-
+  
   pinMode(TRIG_PIN, OUTPUT);
   pinMode(ECHO_PIN, INPUT);
-
+  
   pinMode(IR_SENSOR_1, INPUT);
   pinMode(IR_SENSOR_2, INPUT);
-
+  
+  pinMode(LED_LEFT, OUTPUT);     // Initialize left LED pin
+  pinMode(LED_RIGHT, OUTPUT);    // Initialize right LED pin
+  
   myServo.attach(SERVO_PIN);
   myServo.write(90);
-
+  
   Serial.begin(9600);
 }
 
 void loop() {
   long distance = measureDistance();
-
   bool irObstacle1 = digitalRead(IR_SENSOR_1);
   bool irObstacle2 = digitalRead(IR_SENSOR_2);
+
+  // Reset LED indication
+  digitalWrite(LED_LEFT, LOW);
+  digitalWrite(LED_RIGHT, LOW);
 
   if (irObstacle1 == LOW || irObstacle2 == LOW) {
     stopMotors();
@@ -56,12 +65,28 @@ void loop() {
   else if (distance < 60) {
     moveForward();
     startServo();
+    digitalWrite(LED_LEFT, LOW);
+    digitalWrite(LED_RIGHT, HIGH);  // Indicate moving forward
     Serial.println("Object within range (Ultrasonic), moving forward.");
   } 
   else {
     stopMotors();
     stopServo();
+    digitalWrite(LED_LEFT, LOW);
+    digitalWrite(LED_RIGHT, LOW);  // No movement, turn off LEDs
     Serial.println("Nothing in front, stopping.");
+  }
+
+  // Additional movement logic: Left and Right based on conditions
+  if (irObstacle1 == LOW) {
+    moveLeft();
+    digitalWrite(LED_LEFT, HIGH);  // Indicate turning left
+    Serial.println("Obstacle detected on left, turning left.");
+  } 
+  else if (irObstacle2 == LOW) {
+    moveRight();
+    digitalWrite(LED_RIGHT, HIGH); // Indicate turning right
+    Serial.println("Obstacle detected on right, turning right.");
   }
 
   delay(100);
@@ -73,7 +98,7 @@ long measureDistance() {
   digitalWrite(TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(TRIG_PIN, LOW);
-
+  
   long duration = pulseIn(ECHO_PIN, HIGH);
   long distance = duration * 0.034 / 2;
   return distance;
@@ -83,9 +108,29 @@ void moveForward() {
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   analogWrite(ENA, 255);
-
+  
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
+  analogWrite(ENB, 255);
+}
+
+void moveLeft() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);  // Reverse left motor direction
+  analogWrite(ENA, 255);
+  
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENB, 255);     // Right motor moving forward
+}
+
+void moveRight() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);    // Left motor moving forward
+  analogWrite(ENA, 255);
+  
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);   // Reverse right motor direction
   analogWrite(ENB, 255);
 }
 
